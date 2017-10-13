9683da5a70fb1d498e8c57a450e3a2b5635fc155
Chema <jose.rodriguez@scmspain.com>
2017-10-13 13:05:22 +0200

    initial comit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+module.exports = {
+  resolve: {
+    extensions: ['.js']
+  },
+  entry: {
+    app: './src/app.js',
+  },
+  output: {
+    path: path.resolve(__dirname,'dist/'),
+    filename: '[name].js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.js|.jsx)$/,
+        exclude: '/node_modules/',
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015','react']
+        }
+      }
+    ]
+  }
+}
