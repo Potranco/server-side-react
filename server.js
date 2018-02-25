74ab9fa1f302e0b6c560451cd5b8e3b2f615541c
Chema <chemar@gmail.com>
2018-02-25 14:39:49 +0100

    init

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 import express from 'express';
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import MyComponent from './dist/MyComponent.js';
 
 const app = express();
 
 app.use('/assets',express.static('dist'));
 
 app.get('/', function (req, res) {
-  let componente = ReactDOMServer.renderToString(<MyComponent version='Servidor'/>);
+  let componente = 'server code';
   const html = `
     <html>
     <head>
@@ -19,7 +16,7 @@ app.get('/', function (req, res) {
       <div id="serverside">
         ${componente}
       </div>
-      <div id="app">Render in client</div>
+      <div id="app">Code client</div>
       <script src="/assets/app.js"></script>
     </body>
     </html>
